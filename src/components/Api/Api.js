3a75e12460cc12a81c456eb6e8c7a0b4997ca182
AntonyZP <106816172+AntonyZP@users.noreply.github.com>
2023-02-10 17:32:40 +0200

    Update Api.js

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -7,7 +7,7 @@ const APIfetchImages = async(
     searchQuery, 
     page = 1
     ) => {
-    const response = await axios.get(`/?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
+    const response = await axios.get(`?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
     return response.data
 }
 
